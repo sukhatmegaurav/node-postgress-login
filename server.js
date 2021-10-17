cab3536973d2f98ee0c783f4d82aabb0e759a33b
Gaurav Sukhatme (gsukhatm) <gsukhatm@cisco.com>
2021-10-17 15:03:16 +0530

    login and register frontend

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express()
+
+const PORT = process.env.PORT || 4000
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.get('/users/register', (req, res) => {
+    res.render("register")
+});
+
+app.get('/users/login', (req, res) => {
+    res.render("login")
+});
+
+app.get('/users/dashboard', (req, res) => {
+    res.render("dashboard", { user: "Lilth" });
+});
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
